330c95058298767ae437494e22a84854c5e8a22a
andreylpereira <andreylpereira@gmail.com>
2025-03-31 10:51:49 -0300

    [FIX] Spring security - Error de autorizacao no ponto de login

--- src/main/java/com/senai/api/security/SecurityConfig.java
+++ src/main/java/com/senai/api/security/SecurityConfig.java
@@ -45,8 +45,8 @@ public class SecurityConfig {
             .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
             .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
-                .requestMatchers("/api/auth/admin/**").permitAll() 
                 .requestMatchers("/api/auth/login/**").permitAll() 
+                .requestMatchers("/api/auth/admin/**").permitAll() 
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers("/api/usuario/lista/**").hasAnyAuthority(Perfil.FUNCIONARIO.getDescricao(), Perfil.ADMINISTRADOR.getDescricao())
                 .requestMatchers("/api/usuario/atualizarSenha/**").hasAnyAuthority(Perfil.FUNCIONARIO.getDescricao(), Perfil.ADMINISTRADOR.getDescricao())
