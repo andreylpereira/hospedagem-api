0dc615fbba55c7234bafc58f399c239c30332d2a
Andrey Luiz Pereira <andreylpereira@gmail.com>
2025-04-01 11:57:25 -0300

    [FIX] Alteração da Query de reservas para evitar falso positivo.

--- src/main/java/com/senai/api/repository/ReservaRepository.java
+++ src/main/java/com/senai/api/repository/ReservaRepository.java
@@ -13,15 +13,15 @@ import com.senai.api.models.Reserva;
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
-	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId "
-			+ "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') "
-			+ "AND r.dataInicio < :dataFim AND r.dataFim > :dataInicio")
-	boolean existsReservaConflitante(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim);
+	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
+		       "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " +
+		       "AND (r.data_inicio < :dataFim AND r.data_fim > :dataInicio)")
+		boolean existsReservaConflitante(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim);
 
-	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId "
-			+ "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " + "AND r.id != :reservaId "
-			+ "AND r.dataInicio < :dataFim AND r.dataFim > :dataInicio")
-	boolean existsReservaConflitanteParaEdicao(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim,
-			Integer reservaId);
-
+	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
+		       "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " +
+		       "AND r.id != :reservaId " +
+		       "AND r.data_inicio < :dataFim AND r.data_fim > :dataInicio")
+		boolean existsReservaConflitanteParaEdicao(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim,
+		                                            Integer reservaId);
 }
