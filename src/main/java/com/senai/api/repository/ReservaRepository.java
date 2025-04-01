0c1a5126996a3c281fd5b3bcf2b2c9b08d2a43be
Andrey Luiz Pereira <andreylpereira@gmail.com>
2025-04-01 13:03:10 -0300

    [FIX] Corrigir mapeamento do dataInicio e dataFim

--- src/main/java/com/senai/api/repository/ReservaRepository.java
+++ src/main/java/com/senai/api/repository/ReservaRepository.java
@@ -13,15 +13,16 @@ import com.senai.api.models.Reserva;
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
-	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
+	@Query("SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
 		       "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " +
 		       "AND (r.data_inicio < :dataFim AND r.data_fim > :dataInicio)")
 		boolean existsReservaConflitante(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim);
 
-	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
+		@Query("SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
 		       "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " +
 		       "AND r.id != :reservaId " +
 		       "AND r.data_inicio < :dataFim AND r.data_fim > :dataInicio")
 		boolean existsReservaConflitanteParaEdicao(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim,
 		                                            Integer reservaId);
+
 }
