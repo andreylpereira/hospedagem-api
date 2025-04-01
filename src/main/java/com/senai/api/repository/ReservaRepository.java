d956837e13bcd467ea19a0e7faa1e544483823d3
Andrey Luiz Pereira <andreylpereira@gmail.com>
2025-04-01 11:22:15 -0300

    [FIX] Query Reservas

--- src/main/java/com/senai/api/repository/ReservaRepository.java
+++ src/main/java/com/senai/api/repository/ReservaRepository.java
@@ -13,16 +13,15 @@ import com.senai.api.models.Reserva;
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 
-    @Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
-           "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " +
-           "AND ((r.dataInicio BETWEEN :dataInicio AND :dataFim) OR (r.dataFim BETWEEN :dataInicio AND :dataFim))")
-    boolean existsReservaConflitante(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim);
+	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId "
+			+ "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') "
+			+ "AND r.dataInicio < :dataFim AND r.dataFim > :dataInicio")
+	boolean existsReservaConflitante(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim);
 
-    @Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId " +
-           "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " +
-           "AND r.id != :reservaId " +
-           "AND ((r.dataInicio BETWEEN :dataInicio AND :dataFim) OR (r.dataFim BETWEEN :dataInicio AND :dataFim))")
-    boolean existsReservaConflitanteParaEdicao(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim, Integer reservaId);
+	@Query("SELECT COUNT(r) > 0 FROM Reserva r WHERE r.acomodacao.id = :acomodacaoId "
+			+ "AND r.status NOT IN ('CONCLUIDO', 'CANCELADO') " + "AND r.id != :reservaId "
+			+ "AND r.dataInicio < :dataFim AND r.dataFim > :dataInicio")
+	boolean existsReservaConflitanteParaEdicao(Integer acomodacaoId, LocalDateTime dataInicio, LocalDateTime dataFim,
+			Integer reservaId);
 
-
-   }
+}
